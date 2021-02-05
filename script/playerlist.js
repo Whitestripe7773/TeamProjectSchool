174ca7d90e731eead246bd4bb02ace1169d1504b
Jan Zurek <janzurek@gmx.net>
2021-02-05 14:35:23 +0100

    tried to implement scoreboard function

---
+++ script/playerlist.js
@@ -0,0 +1,14 @@
+$("document").keypress(function(event){
+    
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        alert('You pressed a "enter" key in somewhere');	
+    }
+    
+});
+
+function spielerListeAnzeigen(){
+}
+function rangAktualisieren(){
+
+}
